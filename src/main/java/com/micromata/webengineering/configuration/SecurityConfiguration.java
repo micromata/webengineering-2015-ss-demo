b937d91f28637e01f4fc19016cd1813285eb4462
Michael Lesniak <mlesniak@micromata.de>
2015-05-22 15:58:56 +0200

    Dummy authentication.

--- src/main/java/com/micromata/webengineering/configuration/SecurityConfiguration.java
+++ src/main/java/com/micromata/webengineering/configuration/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.micromata.webengineering.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -21,5 +22,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     ;
   }
 
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
 
+        .inMemoryAuthentication()
+        .withUser("user")
+        .password("foo")
+        .roles("USER")
+    ;
+
+  }
 }
