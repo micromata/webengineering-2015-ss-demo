c70f19d9bc2cf276a3205d035779406aab87ab04
Michael Lesniak <mlesniak@micromata.de>
2015-05-22 16:38:17 +0200

    Authentification over database.

--- src/main/java/com/micromata/webengineering/configuration/SecurityConfiguration.java
+++ src/main/java/com/micromata/webengineering/configuration/SecurityConfiguration.java
@@ -1,15 +1,29 @@
 package com.micromata.webengineering.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 /**
  * @author Michael Lesniak (mlesniak@micromata.de)
  */
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+  private static final Logger LOG = LoggerFactory.getLogger(SecurityConfiguration.class);
+
+  @Autowired
+  private DataSource dataSource;
+
+  @Autowired
+  private Environment environment;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
@@ -22,13 +36,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth
+    if (environment.acceptsProfiles("heroku") == false) {
+      LOG.warn("Adding default user:foo");
+      auth
+          .inMemoryAuthentication()
+          .withUser("user")
+          .password("foo")
+          .roles("USER")
+      ;
 
-        .inMemoryAuthentication()
-        .withUser("user")
-        .password("foo")
-        .roles("USER")
-    ;
+      auth
+          .jdbcAuthentication()
+          .dataSource(dataSource)
+          .usersByUsernameQuery("select username,password,'true' from user where username = ?")
+          .authoritiesByUsernameQuery("select username,'ROLE_USER' from user where username = ?")
+      ;
+    }
+
 
   }
 }
